6b9e43a137cb507928df5a4830d993f2820cf9a6
ReetuChauhann <chauhanreetu00@gmail.com>
2022-04-22 22:05:35 +0530

    Swap

---
+++ Qus1.java
@@ -0,0 +1,18 @@
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+
+public class Qus1 {
+    public static void main(String[] args) {
+        int[] arr = {1,3,23,9,18};
+        System.out.println(Arrays.toString(arr));
+        swap(arr,1,4);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    static void swap(int[] arr, int index1, int index2){
+        int temp;
+        temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
