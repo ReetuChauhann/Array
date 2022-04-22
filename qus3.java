2fe6137ec9a08331321393de726378cb47050188
ReetuChauhann <chauhanreetu00@gmail.com>
2022-04-22 22:36:55 +0530

    reverse

---
+++ qus3.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class qus3 { //reverse the elements of an array!
+    public static void main(String[] args) {
+        int[] arr = {1,3,23,9,18};
+        System.out.println(Arrays.toString(arr));
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[] arr){
+       int start = 0;
+       int end = arr.length-1;
+       while (start<end) {
+           swap(arr,start,end);
+           start++;
+           end--;
+        }
+     }
+
+     static void swap(int[] arr, int start, int end){
+         int temp;
+         temp = arr[start];
+         arr[start] = arr[end];
+         arr[end] = temp;
+     }
+}
