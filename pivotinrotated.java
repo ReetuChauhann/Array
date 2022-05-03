6ab5a11b49a2a5078c84b799705f1aa7ea4116ac
ReetuChauhann <chauhanreetu00@gmail.com>
2022-05-03 13:15:26 +0530

    pivot in rotated array

---
+++ pivotinrotated.java
@@ -0,0 +1,30 @@
+public class pivotinrotated {
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,6,7,0,1,2};
+        int ans =searchpivot(arr);
+        System.out.println(ans);
+
+    }
+
+    static int searchpivot(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            if(arr[mid]>arr[mid+1] && mid>end){
+                return mid;
+            }
+            if(arr[mid]<arr[mid-1] && mid>start){
+                return mid-1;
+            }
+            if(arr[start]>arr[mid]){
+                end = mid-1;
+            }
+            if(arr[start]<arr[mid]){
+                start = mid+1;
+            }
+        }
+    return -1;
+
+    }
+}
